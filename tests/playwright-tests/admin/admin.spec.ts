b7320810f0c2ea11620c76181b34d4ca33e81cc2
Aman deep sharma <amandeepsharma.qlo548@webkul.in>
2025-05-13 20:43:51 +0530

    Fixed issue with workflow

--- tests/playwright-tests/admin/admin.spec.ts
+++ tests/playwright-tests/admin/admin.spec.ts
@@ -11,4 +11,24 @@ test('AdminLogin Page', async ({ page }) => {
     // Verify that it is an Authentication Page
     await expect(page.getByRole('heading', { name: 'QloApps' })).toBeVisible();
   });
+
+  test('fill admin login form and submit', async ({ page }) => {
+    await page.goto(QLOAPPS_ADMIN_URL);
+  
+    // Fill email
+    await page.fill('#email', 'admin@example.com');
+  
+    // Fill password
+    await page.fill('#passwd', 'admin1234');
+  
+    // Optionally check "Stay logged in"
+    await page.check('#stay_logged_in');
+  
+    // Click the login button
+    await page.click('button[name="submitLogin"]');
+  
+    // Optional: Verify navigation to the dashboard or any successful login indicator
+    await expect(page).toHaveURL(/.*AdminDashboard.*/);
+  });
+    
   
