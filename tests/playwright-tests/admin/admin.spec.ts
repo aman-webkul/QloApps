4c6cfc49fac342ad18212166fc3f34c219e3cc98
Aman deep sharma <amandeepsharma.qlo548@webkul.in>
2025-05-13 17:10:05 +0530

    fixe issue

--- tests/playwright-tests/admin/admin.spec.ts
+++ tests/playwright-tests/admin/admin.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-const QLOAPPS_ADMIN_URL = process.env.ADMIN_BASE_URL || 'http://127.0.0.1/QloApps_play/adminhtl';
+const QLOAPPS_ADMIN_URL = process.env.ADMIN_BASE_URL || 'http://127.0.0.1/QloApps/adminhtl';
 
 test('AdminLogin Page', async ({ page }) => {
+    console.log(QLOAPPS_ADMIN_URL);
     await page.goto(QLOAPPS_ADMIN_URL);
   
     await expect(page).toHaveTitle(/Administration panel/);
--- tests/playwright-tests/website/website.spec.ts
+++ tests/playwright-tests/website/website.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-const QLOAPPS_URL = process.env.WEBSITE_BASE_URL || 'http://127.0.0.1/QloApps_play';
+const QLOAPPS_URL = process.env.WEBSITE_BASE_URL || 'http://127.0.0.1/QloApps';
 
 test('Home Page Check', async ({ page }) => {
+  console.log(QLOAPPS_URL);
   await page.goto(QLOAPPS_URL);
 
   await expect(page).toHaveTitle(/Hotel Prime/);
