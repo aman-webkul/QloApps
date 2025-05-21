50c0f0dcbaa77790b44232689c5473b6a54ce821
Aman deep sharma <amandeepsharma.qlo548@webkul.in>
2025-05-21 17:51:27 +0530

    Change testcase dlow

--- tests/playwright-tests/website/website.spec.ts
+++ tests/playwright-tests/website/website.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test';
 const QLOAPPS_URL = process.env.WEBSITE_BASE_URL || 'http://127.0.0.1/QloApps';
 
 test('Home Page Check', async ({ page }) => {
-  console.log(QLOAPPS_URL);
   await page.goto(QLOAPPS_URL);
 
   await expect(page).toHaveTitle(/Hotel Prime/);
--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -3,6 +3,10 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
 	testDir: './playwright-tests',
 
+    timeout: 120 * 1000,
+
+    expect: { timeout: 20 * 1000 },
+
     fullyParallel: false,
 
     forbidOnly: !!process.env.CI,
@@ -12,6 +16,7 @@ export default defineConfig({
     workers: 1,
 
     use: {
+        baseURL: 'http://127.0.0.1/QloApps',
 		screenshot: { mode: 'only-on-failure', fullPage: true },
         video: 'retain-on-failure',
         trace: 'retain-on-failure',
